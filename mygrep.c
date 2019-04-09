aa0cb4fb11e70b185608e7c8dc05e2769a5d3c9d
ovidiucs <ovidiu@bit0.ro>
2019-04-09 21:09:34 +0300

    new commit

--- mygrep.c
+++ mygrep.c
@@ -5,12 +5,12 @@
 const char *myarg = NULL;
 
 int readfile() {
-	char *ret;
-	const char searchedTerm[20] = "This";
-	const char textsearched[100] = "This is free software: you are free to change and redistribute it.";
+	char* ret;
+	char s2[20] = "This";
+	char s1[100] = "This is free software: you are free to change and redistribute it.";
 
-	ret = strstr(textsearched,searchedTerm);
-	printf("The substring is: %s\n");
+	ret = strstr(s1,s2);
+	printf("The substring is: %s s1 %s s2 %s ret\n",s1,s2,ret);
 
 	return 0;
 }
@@ -18,10 +18,11 @@ int readfile() {
 int main(int argc, char *argv[]) {
 
 // Declare pointer to file for use later
+#if 0
 	FILE *fptr;
 	char ch;
 	myarg = argv[1];
-#if 0
+	
 	if (argc == 3 ){
 		printf("The search term is %s and the filename is '%s'\n", argv[1], argv[2]);
 		fptr = fopen (argv[2], "r");
@@ -29,25 +30,25 @@ int main(int argc, char *argv[]) {
 		printf("Usage: Specify a search term followed by a filename.\n");
 		exit(0);
 	}
-#endif
+
 
 	if (fptr == NULL) {
 	// Empty file	
 			printf("Unable to open specified file\n");
 			exit(1);
 	} else {
+#endif
 		//int rc = readfile(fptr,argv[1]);
 		int rc = readfile();
-		printf ("%s",rc);
+		printf (rc);
 #if 0
 		// fgetc - return the character currently pointed by the
 		// internal file position. internal file position is then adv
 		// anced to the next character
 			while((ch=fgetc(fptr))!=EOF) {
 			printf("%c", ch);
-#endif
 	}	
 	fclose(fptr);
+#endif
 	return 0;
 }
-
