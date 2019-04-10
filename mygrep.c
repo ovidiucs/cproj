b1f8e60c166c2af1cc9e288b120446b2bd02c24a
ovidiucs <ovidiu@bit0.ro>
2019-04-10 15:13:53 +0300

    read from file test

--- mygrep.c
+++ mygrep.c
@@ -5,11 +5,17 @@
 const char *myarg = NULL;
 
 int readfile(char *argument) {
-	printf("%s\n", argument);
-	char* ret;
-	char* s2 = argument;
-	char s1[100] = "This is free software: you are free to change and redistribute it.";
 
+	char* ret;
+	// if (( *fptr = fopen(argument,"r")) == NULL) 
+	// {
+	// // Empty file	
+	// 		printf("Unable to open specified file\n");
+	// 		exit(1);
+	// }
+	//printf(fptr);
+	char* s2 = argument;
+	char s1[] = "test";
 	ret = strstr(s1,s2);
 	printf("The substring in s1 is %s and in s2 is %s and the returned is %s\n",s1,s2,ret);
 
@@ -19,29 +25,31 @@ int readfile(char *argument) {
 int main(int argc, char *argv[]) {
 
 // Declare pointer to file for use later
-#if 0
+
 	FILE *fptr;
-	char ch;
+	char ch[] = "";
 	myarg = argv[1];
-	
-		printf("The search term is %s and the filename is '%s'\n", argv[1], argv[2]);
+	if (argc == 3) {
+		printf("The search term is '%s' and the filename is '%s'\n", argv[1], argv[2]);
 		fptr = fopen (argv[2], "r");
 	} else {
 		printf("Usage: Specify a search term followed by a filename.\n");
 		exit(0);
 	}
 
-
 	if (fptr == NULL) {
 	// Empty file	
 			printf("Unable to open specified file\n");
 			exit(1);
 	} else {
-#endif
+
 		//int rc = readfile(fptr,argv[1]);
 		char *argument = argv[1];
+		
+		puts(fgets (ch,60, fptr));
 		int rc = readfile(argument);
 		//printf (rc);
+	}
 #if 0
 		// fgetc - return the character currently pointed by the
 		// internal file position. internal file position is then adv
