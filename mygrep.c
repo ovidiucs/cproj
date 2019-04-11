19d18b4dc99f99a9c7c33838819755c7f931336f
ovidiucs <ovidiu@bit0.ro>
2019-04-11 15:24:48 +0300

    loop through the text

--- mygrep.c
+++ mygrep.c
@@ -2,24 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 
-//const char *myarg = NULL;
-
 int readfile(char *argument, FILE **fptr) {
 	char data[1000];
 	char* ret;
-	//char* s1 = *fptr;
 	char* s2 = argument;
 
-	while(fgets (data,1000,*fptr) != NULL);
+	while(fgets (data,1000,*fptr) != NULL) {
 	ret = strstr(data,s2);
+	if (ret != NULL) {
 	printf("The substring in s1 is %s and in s2 is %s and the returned is %s\n",data,s2,ret);
-
+		}
+	}
 	return 0;
 }
 
 int main(int argc, char **argv) {
-
-// Declare pointer to file for use later
 
 	FILE *fptr;
 	char* myarg = argv[1];
@@ -32,24 +29,12 @@ int main(int argc, char **argv) {
 	}
 
 	if (fptr == NULL) {
-	// Empty file
 			printf("Unable to open specified file\n");
 			exit(1);
 	} else {
 
-		//int rc = readfile(fptr,argv[1]);
 		int rc = readfile(myarg, &fptr);
-		//printf (rc);
 	}
 
-#if 0
-		// fgetc - return the character currently pointed by the
-		// internal file position. internal file position is then adv
-		// anced to the next character
-			while((ch=fgetc(fptr))!=EOF) {
-			printf("%c", ch);
-	}
-	fclose(fptr);
-#endif
 	return 0;
 }
