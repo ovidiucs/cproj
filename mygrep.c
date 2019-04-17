42e3c46ee0935536ed83430360516b473190b4ed
Ovidiu <ovidiu@bit0.ro>
2019-04-17 08:36:56 +0000

    commit

--- mygrep.c
+++ mygrep.c
@@ -1,40 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-int readfile(char *argument, FILE **fptr) {
+int readfile(char *pat, FILE *fptr, char *fn ) {
 	char data[1000];
 	char* ret;
-	char* s2 = argument;
+	char* s2 = pat;
 
-	while(fgets (data,1000,*fptr) != NULL) {
-	ret = strstr(data,s2);
-	if (ret != NULL) {
-	printf("The substring in s1 is %s and in s2 is %s and the returned is %s\n",data,s2,ret);
+	while(fgets (data,sizeof(data),fptr) != NULL) {
+		ret = strstr(data,s2);
+		if (ret != NULL) {
+		printf("%s:\n",fn,data,s2,ret);
+		//	fputs(data, stdout);
 		}
+	}
+	if (ferror(fptr)) {
+		fprintf(stdout,"Error reading file %s\n", strerror(errno));
+		exit(1);
 	}
 	return 0;
 }
 
 int main(int argc, char **argv) {
-
+	
 	FILE *fptr;
-	char* myarg = argv[1];
-	if (argc == 3) {
+	char* fn;
+	const char* pat = argv[1];
+	int rc = 0;
+	
+	if (argc >= 3) {
 		printf("The search term is '%s' and the filename is '%s'\n", argv[1], argv[2]);
-		fptr = fopen (argv[2], "r");
+		for ( int i = 2; i < argc; i++ ) {
+			fn = argv[i];	
+			fptr = fopen (fn, "r");
+			if (fptr == NULL) {
+				fprintf(stderr,"Unable to open %s\n", fn);
+				exit(1);
+			} else {
+				rc = readfile(pat, fptr, fn);
+			}
+			fclose(fptr);
+		}
 	} else {
 		printf("Usage: Specify a search term followed by a filename.\n");
 		exit(0);
 	}
 
-	if (fptr == NULL) {
-			printf("Unable to open specified file\n");
-			exit(1);
-	} else {
-
-		int rc = readfile(myarg, &fptr);
-	}
-
-	return 0;
+	return rc;
 }
