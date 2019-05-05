bb287fbfa3ea40c808f9d4b8fb8695fd64aea06c
ovidiucs <ovidiu@bit0.ro>
2019-05-05 16:03:35 +0300

    qsort 1

--- llist.c
+++ llist.c
@@ -15,91 +15,56 @@ typedef struct Node {
 } Node;
 
 static Node* head;
-// Node* tail;
+static Node* tail;
 
 static void saveline(char *data) {
 
 	// initialize return value, a temporary Node and a previous node
 	int ret = 0;
+	int counter = 0;
 	Node* tempNode;
 	Node* prevNode;
-	fprintf(stderr, "Function called\n");
 	// Inital node
 	Node* nodePointer;
 
 	// Allocate memory - malloc returns a generic pointer.
-	// We then cast Node* such that malloc has a base type of Node instead of generic void*.
 	nodePointer = (Node*) malloc (sizeof(Node) + strlen(data));
-	fprintf(stderr, "Malloc called on nodePointer %p\n", nodePointer);
 
 	// set nodePointer->next member to NULL
 	nodePointer->next = NULL;
-	fprintf(stderr, "Set nodePointer->next to %p\n", nodePointer);
+
 
 	// Copy bytes to struct memeber 'text'
 	strcpy(nodePointer->text,data);
-
-	// Insert node in list in the right place in sorted order
-
-	// 1. If head Node is NULL then set head to nodePointer;  - save nodePointer address
 	if (head == NULL) {
-		head = nodePointer;
-				fprintf(stderr, "Head is null so set it to: %p \n",head );
-	// 2. Otherwise - head already is set. Now set previous Node to NULL to keep track of
-	// previously traversed nodes. - temp variable.
+		head = tail = nodePointer;
 	} else {
-	prevNode = NULL;
-	// 3. assign head to temp node and while it's not null then assign the
-	// address of the next element in the list to temp node.
-		for (tempNode=head; tempNode!=NULL ; tempNode=tempNode->next){
-		fprintf(stderr, "Head is: %p\t tempNode is: %p\t tempNode->next is: %p\n",
-				head, tempNode, tempNode->next );
-	// 4. compare string pointed to by nodePointer and tempNode
-			ret = strcmp(nodePointer->text, tempNode->text);
-	// 5. if the comparsion returns less than or equal to 0
-			if (ret <= 0) {
-	// 5a. if the previous node is NULL
-				if (prevNode == NULL) {
-	// 5b. then set head to nodePointer
-						fprintf(stderr, "Head is: %p\n ", head);
-
-					head = nodePointer;
-				} else {
-	// 5c. otherwise set the prevNode->next to nodePointer
-					prevNode->next = nodePointer;
-					fprintf(stderr, "\nInner PrevNode->next is: %p\n ", prevNode->next);
-				}
-				// end if prevNode == NULL
-	// 6.
-				  nodePointer->next=tempNode;
-				  fprintf(stderr, "NodePointer->next is: %p\n ", nodePointer->next);
-				return;
-			}
-			// end if ret <= 0
-			prevNode = tempNode;	
-			fprintf(stderr, "	PrevNode is: %p\n ", prevNode);
-		 }
-		 // end for loop
-	prevNode->next = nodePointer;
-	fprintf(stderr, "PrevNode->next is: %p\n ", prevNode->next);
+		tail->next = nodePointer;
+		tail = nodePointer;
 	}
-	// emd else
-	fprintf(stderr, "End of function\n");
-	fprintf(stderr, "____________________\n");
 }
 
 static int  readfile( FILE *fptr, char *fname ) {
+
 	char data[1000];
-	int rc = 0;
+	int counter = 0;
 
 	while(fgets (data,(int)sizeof(data),fptr) != NULL) {
 		saveline(data);
+		counter +=1;
 	}
-	return rc;
+	return counter;
 }
+
+static int compar (const void *a,  const void *b) {
+		//https://cboard.cprogramming.com/c-programming/64520-sorting-linked-list-qsort.html#post458752
+    return strcmp(((Node*)a)->text, ((Node*)b)->text);
+}
+
 int main(int argc, char** argv){
 
 	int rc = 0;
+	int counter = 0;
 	FILE* fptr;
 	char* fname;
 	int numNode = 0;
@@ -113,7 +78,7 @@ int main(int argc, char** argv){
 			fprintf(stderr,"Unable to open %s\n", fname);
 			rc = 1;
 		} else {
-			rc = readfile(fptr, fname);
+			counter = readfile(fptr, fname);
 		}
 		rc = fclose (fptr);
 	} else if (argc == 1){
@@ -124,22 +89,30 @@ int main(int argc, char** argv){
 		rc = 0;
 	}
 	tempNode=head;
-
+	Node* arrayNode[counter];
 	Node* prevNode;
 
-	for (;;) {
-		fprintf(stderr,"Node#: %d, at mem. address: %p with sizeof line: %d data is: %s",
-						numNode, (void *) tempNode, (int)strlen(tempNode->text),tempNode->text);
+	for (tempNode=head;tempNode!=NULL;tempNode=tempNode->next) {
+		fprintf(stderr,"Node#: %d, allocated mem. address: %p with strlen of text: %d data is: %s",
+						numNode, (Node *) tempNode, (int)strlen(tempNode->text),tempNode->text);
+		arrayNode[numNode] = tempNode;
+
 		numNode += 1;
-		prevNode = tempNode->next;
-		free((void *) tempNode);
-		if (prevNode == NULL) {
-			break;
-		} else {
-			tempNode = prevNode;
-		}
+	}
+				int i = 0;
+
+		for (i=0;i<10;i++) {
+		fprintf(stderr,"\nArray loop: %d memory address of text %p with string (char *) %s",i, arrayNode[i]->text,arrayNode[i]->text);
+					qsort ( (void *)	arrayNode,
+					10,
+					sizeof(arrayNode)/sizeof(arrayNode[i]),
+					compar);
+	}
+		for (i=0;i<10;i++) {
+		fprintf(stderr,"\tArray loop: %d memory address of text %p with string (char *) %s\n",i, arrayNode[i]->text,arrayNode[i]->text);
 	}
 		return rc;
 
-}
+	}
+//}
 
