52927fdd1362fc91dcadc7f29b16079a80bb0519
ovidiucs <ovidiu@bit0.ro>
2019-05-07 09:10:32 +0300

    llist v1

--- llist.c
+++ llist.c
@@ -4,6 +4,14 @@
 #include <errno.h>
 #include <stddef.h>
 
+
+typedef struct bst {
+	struct bst *bleft;
+	struct bst *bright;
+	char *btext;
+
+} bst;
+
 typedef struct Node {
 
 	// store pointer address of next block of data - 8 bytes / 64 bit - 4 bytes / 32 bit arch
@@ -44,6 +52,38 @@ static void saveline(char *data) {
 	}
 }
 
+static void bstSaveLine(char *data) {
+
+	// initialize return value, a temporary Node and a previous node
+	int result = 0;
+	int counter = 0;
+
+	// Inital node
+	bst* rootBstPointer;
+
+	// Allocate memory - malloc returns a generic pointer.
+	rootBstPointer = (bst*) malloc (sizeof(bst));
+	rootBstPointer->btext = (char*) malloc(sizeof(data));
+
+	// set nodePointer->next member to NULL
+	rootBstPointer->bleft = NULL;
+	rootBstPointer->bright = NULL;
+
+
+	// Copy bytes to struct memeber 'text'
+	result = strcmp(rootBstPointer->data);
+
+
+	if (result == 0) {
+		 rootBstPointer = rootBstPointer;
+	} else if (result < 0 ) {
+		return rootBstPointer->bleft;
+	} else {
+		return rootBstPointer->bright;
+	}
+
+}
+
 static int readfile( FILE *fptr, char *fname ) {
 
 	char data[1000];
@@ -58,11 +98,11 @@ static int readfile( FILE *fptr, char *fname ) {
 
 static int compar (const void *a,  const void *b) {
 		//https://cboard.cprogramming.com/c-programming/64520-sorting-linked-list-qsort.html#post458752
-		
+
     int i = strcmp( (**( const Node**)a).text, (*( const Node**)b)->text );
     fprintf(stderr,"%d and string A %p and string B %p\n", i,a,b);
     return i;
-} 
+}
 	// Function main executes the main body of the program. Arguments: argc and argv. Function  main executes the main body of the program.  Arguments: argc and argv.  Function main
 	 // executes the main body of the program.  Arguments: argc and argv.
 int main(int argc, char** argv){
@@ -119,6 +159,6 @@ int main(int argc, char** argv){
 		return rc;
 
 	}
-	
+
 //}
 
