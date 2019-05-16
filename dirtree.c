349a0997a438a636cf25fe06d497cf4bef2160bb
ovidiucs <ovidiu@bit0.ro>
2019-05-16 22:10:43 +0300

    newdir 1

--- dirtree.c
+++ dirtree.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <dirent.h>
 
-static void dirPrint(dirpath, int indentLevel) {
+static void dirPrint(char *dirpath, int indentLevel) {
 	// read directory entries
-
+	DIR *dirptr;
+	dirptr = opendir(dirpath);
+	struct dirent *ent;
+	if (dirptr != 0)
+		perror(NULL);
 	// header print
-	// Directory tree of X
+	fprintf(stdout,"Directory tree of %s\n-----------------------\n", dirpath);
 	// for each directory entry
+	while ( (ent = readdir(dirptr) != NULL ) ) {
+	fprintf(stdout,"%s\n",readdir(dirptr)->d_name);
 		// print file to stdout
 			// if file is a directory increase indentation (add space)
 			// rerund same function with argument now as current directory
-
+		}
+	closedir(dirptr);
 }
+
 int main (int argc, char **argv) {
 	int rc = 0;
 	int ret = 0;
@@ -31,8 +40,13 @@ int main (int argc, char **argv) {
 		if (ret != 0)
 			perror(NULL);
 		else {
-			if ( fstat.st_mode & S_IFDIR )
+			if ( fstat.st_mode & S_IFDIR ) {
 				fprintf(stderr, "This is a directory\n");
+				fprintf(stderr, "Calling opendir\n");
+				dirPrint(argv[1],1);
+		}
+
+
 		}
 		// rest of code
 	// call
