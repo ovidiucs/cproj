12ff7c64e3b355b0753b087ee4bc0cb7f0e9bb12
ovidiucs <ovidiu@bit0.ro>
2019-05-16 08:42:32 +0300

    dirtree - print .1

--- dirtree.c
+++ dirtree.c
@@ -2,14 +2,21 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
+static void dirPrint(dirpath, int indentLevel) {
+	// read directory entries
 
+	// header print
+	// Directory tree of X
+	// for each directory entry
+		// print file to stdout
+			// if file is a directory increase indentation (add space)
+			// rerund same function with argument now as current directory
 
-
-
+}
 int main (int argc, char **argv) {
 	int rc = 0;
 	int ret = 0;
-	
+
 	struct stat fstat;
 	//fstat->st_mode & S_IFDIR
 	//fprintf(stderr,"Argument count is: %d\n", argc);
@@ -18,7 +25,7 @@ int main (int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s dir\n", argv[0]);
 		rc = 1;
-		
+
 	} else {
 		ret = stat(argv[1],&fstat);
 		if (ret != 0)
@@ -28,7 +35,8 @@ int main (int argc, char **argv) {
 				fprintf(stderr, "This is a directory\n");
 		}
 		// rest of code
+	// call
 	}
 	return rc;
-	
+
 }
