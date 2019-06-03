8c45be3778d1ea93fceabd7e39ec4dd79e630cf5
ovidiucs <ovidiu@bit0.ro>
2019-06-03 06:07:39 +0300

    mycat2

--- mycat.c
+++ mycat.c
@@ -3,22 +3,40 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 
 //
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
+#endif
+
 static int readLn(char *fn, char *flags, char* mode){
 
 }
 
 int main(int argc, char** argv) {
     // check number of arguments
+    //1. Open the file (or create it)
+    // returns integer for the file descriptor,
+    // non negative, takes a char pointer to a filename and flags
+    // a file descriptor is a reference to an open file description
+    int inputFd = 0;
+    int outputFd = 0;
+    int flags = 0;
 
     if(argc > 2) {
         fprintf(stderr,"Usage: %s [filename]\n",argv[0]);
     } else if (argc == 2){
-        fprintf(stderr, "Using file file");
-        inputFd = open(argv[1],)
+        fprintf(stderr, "Using file file: %s\n", argv[1]);
+        inputFd = open(argv[1],O_RDONLY);
+        if(inputFd == -1){
+            errno;
+        }
+        outputFd = read(inputFd,BUF_SIZE,1);
+        
     } else {
-        fprintf(stderr, "reading from stdin");
+        fprintf(stderr, "reading from stdin\n");
     }
+    //
 }
