294913768bcef0ad1ecdf2fb33dcf58bc09bcb76
ovidiucs <ovidiu@bit0.ro>
2019-12-05 07:52:45 +0200

    pthread - create thread

--- graph/db/pthreads/pthread_example.c
+++ graph/db/pthreads/pthread_example.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-
-void foo(){
+// https://youtu.be/uA8X5zNOGw8
+void *foo(void *arg){
   while(1) {
   fprintf(stdout,"My Turn\n");
   sleep(1);
   }
+  return NULL;
 }
 
 void bar(){
   while(1) {
-  fprintf(stdout,"Yout Turn\n");
+  fprintf(stdout,"Your Turn\n");
   sleep(1);
   }
 
 }
 int main (int argc, char** argv) {
+  //1. Create thread.
+  pthread_t newthread;
 
-    foo();
-    bar();
+  //2. args, pthread_t address, NUL
+  pthread_create(&newthread, NULL,foo, NULL);
+
+
+//  foo();
+   bar();
    return 0;
 }
