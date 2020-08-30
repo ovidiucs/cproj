cf905b536fe8540f705421a1f5eb28f4294446e2
Ovidiu <ovidiu@bit0.ro>
2020-08-30 22:47:45 +0300

    sdstest1

--- sdstest/include/square.h
+++ sdstest/include/square.h
@@ -1,2 +1,3 @@
 #include <stdio.h>
 extern int square(int x,int y);
+
--- sdstest/src/main.c
+++ sdstest/src/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include "../lib/sds/sds.h"
-#include "../include/square.h"
+#include "sds/sds.h"
+#include "square.h"
 
 
 int main (int argc, char** argv) {
     if (argc != 2) {
       fprintf(stderr,"Argument count missing\n");
     }
+    
+ sds mystring = sdsnew("Hello World!");
+//    printf("%s\n", mystring);
+//    sdsfree(mystring);
+
     fprintf(stdout, "The number of arguments is: %d\n",argc);
-    fprintf(stdout, "%s\n","correct" ); 
+    fprintf(stdout, "The sds string is %s\n","test" ); 
 
     int rc = square(4,5);
     fprintf(stdout, "%d\n",rc);
